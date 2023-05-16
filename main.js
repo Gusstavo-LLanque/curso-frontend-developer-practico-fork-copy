1b1d084e491f2af47b3615e6db4d5cf95755e3a3
Gustavo LLanque <gusstavollanque@gmail.com>
2023-05-16 15:29:53 -0400

    6to fusion, hicimos lo mismo que la 5ta fusioon arreglar los detalles de las ventanas que mandamos a llamar sin arruinar las otras ventanas, tanto para pc como para mobile.

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ const desktopMenu =document.querySelector('.desktop-menu');
 const menuHamIcon =document.querySelector('.menu');
 const mobileMenu =document.querySelector('.mobile-menu');
 const menuCarritoIcon =document.querySelector('.navbar-shopping-cart')
+const productDetailCloseIcon =document.querySelector('.product-detail-close')
 const shoppingCartContainer =document.querySelector('#shoppingCartContainer')
 const cardsContainer =document.querySelector('.cards-container')
+const productDetailContainer =document.querySelector('#productDetail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu () {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
@@ -28,6 +32,9 @@ function toggleMobileMenu () {
     if (!isAsideClosed){
         shoppingCartContainer.classList.add('inactive')
     }
+
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle('inactive');
 }
 
@@ -38,7 +45,26 @@ function toggleCarritoAside() {
     if (!isMobileMenuClosed){
         mobileMenu.classList.add('inactive')
     }
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
+
+    if (!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive')
+    }
+    
     shoppingCartContainer.classList.toggle('inactive')
+}
+
+
+
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive')
+
+    productDetailContainer.classList.remove('inactive')
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive')
 }
 
 const productList = [];
@@ -65,6 +91,7 @@ for (product of productList) {
 
     const productImg =document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside)
 
     const productInfo =document.createElement('div');
     productInfo.classList.add('product-info');
